54429024219f9deb5a3d785f70c3c0506916aa0f
shapeshed <george@shapeshed.com>
2024-03-20 09:08:10 +0000

    Add handle_redeem (#11)
    * Add handle_redeem
    
    * Update Cargo.toml

--- src/handle.rs
+++ src/handle.rs
@@ -1,10 +1,10 @@
 use crate::config::MyConfig;
 use crate::contract::MyVault;
 use crate::state::MyState;
-use cosmwasm_std::{DepsMut, Env, Event, MessageInfo, Response};
+use cosmwasm_std::{coin, BankMsg, CosmosMsg, DepsMut, Env, Event, MessageInfo, Response};
 use cw_utils::must_pay;
 use osmosis_std::types::cosmos::base::v1beta1::Coin as OsmosisCoin;
-use osmosis_std::types::osmosis::tokenfactory::v1beta1::MsgMint;
+use osmosis_std::types::osmosis::tokenfactory::v1beta1::{MsgBurn, MsgMint};
 use vaultenator::config::Configure;
 use vaultenator::errors::ContractError;
 use vaultenator::handlers::Handle;
@@ -37,8 +37,6 @@ impl Handle<MyConfig, MyState> for MyVault {
         let amount =
             must_pay(&info, &config.base_denom).map_err(|_| ContractError::InvalidFunds {})?;
 
-        // TODO calc strategy share
-
         let strategy_denom = config
             .strategy_denom
             .as_ref()
@@ -57,10 +55,40 @@ impl Handle<MyConfig, MyState> for MyVault {
     }
     fn handle_redeem(
         &self,
-        _deps: DepsMut,
-        _env: Env,
-        _info: MessageInfo,
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
     ) -> Result<Response, ContractError> {
-        unimplemented!()
+        MyState::is_open_and_unpaused(deps.as_ref())?;
+        let config = MyConfig::get_from_storage(deps.as_ref())?;
+
+        let strategy_denom = config
+            .strategy_denom
+            .ok_or(ContractError::DenomNotInitialized {})?;
+
+        let strategy_denom_amount =
+            must_pay(&info, &strategy_denom).map_err(|_| ContractError::InvalidFunds {})?;
+
+        let burn_strategy_token_msg = MsgBurn {
+            sender: env.contract.address.to_string(),
+            amount: Some(OsmosisCoin {
+                denom: strategy_denom.to_string(),
+                amount: strategy_denom_amount.to_string(),
+            }),
+            burn_from_address: env.contract.address.to_string(),
+        };
+
+        let msg_transfer = CosmosMsg::Bank(BankMsg::Send {
+            to_address: info.sender.to_string(),
+            amount: vec![coin(strategy_denom_amount.into(), config.base_denom)],
+        });
+
+        let mut response = Response::default();
+
+        response = response
+            .add_message(burn_strategy_token_msg)
+            .add_message(msg_transfer);
+
+        Ok(response)
     }
 }
--- tests/handle.rs
+++ tests/handle.rs
@@ -17,13 +17,46 @@ fn deposit() {
     let strategy_denom = config.strategy_denom.unwrap();
 
     let signer_strategy_denom_balance =
-        env.get_balance(env.signer.address(), strategy_denom.clone());
+        env.get_balance(&env.signer.address(), &strategy_denom.clone());
 
-    let contract_base_denom_balance = env.get_balance(contract_addr, config.base_denom);
+    let contract_base_denom_balance = env.get_balance(&contract_addr, &config.base_denom);
 
-    let base_denom_total_supply = env.get_total_supply(strategy_denom);
+    let base_denom_total_supply = env.get_total_supply(&strategy_denom);
 
     assert_eq!(base_denom_total_supply, Uint128::from(20_000_000u128));
     assert_eq!(signer_strategy_denom_balance, Uint128::from(20_000_000u128));
     assert_eq!(contract_base_denom_balance, Uint128::from(20_000_000u128));
+}
+
+#[test]
+fn redeem() {
+    let env = TestEnv::new();
+    let wasm = Wasm::new(&env.app);
+    let contract_addr = env.deploy_contract(&wasm);
+
+    // Deposit 20_000_000
+    let amount = coin(20_000_000u128, "uosmo".to_string());
+    env.deposit(&wasm, &contract_addr, amount, &env.signer)
+        .unwrap();
+
+    let config = env.query_config(&wasm, &contract_addr).unwrap();
+    let strategy_denom = config.strategy_denom.unwrap();
+    let signer_strategy_denom_balance =
+        &env.get_balance(&env.signer.address(), &strategy_denom.clone());
+    let contract_base_denom_balance = env.get_balance(&contract_addr, &config.base_denom);
+    let strategy_denom_total_supply = env.get_total_supply(&strategy_denom);
+
+    assert_eq!(strategy_denom_total_supply, Uint128::from(20_000_000u128));
+    assert_eq!(signer_strategy_denom_balance, Uint128::from(20_000_000u128));
+    assert_eq!(contract_base_denom_balance, Uint128::from(20_000_000u128));
+
+    // Redeem 20_000_000
+    let strategy_token_amount = coin(20_000_000u128, strategy_denom.clone());
+    env.redeem(&wasm, &contract_addr, strategy_token_amount, &env.signer)
+        .unwrap();
+    let signer_strategy_denom_balance = env.get_balance(&env.signer.address(), &strategy_denom);
+    let contract_base_denom_balance = env.get_balance(&contract_addr, &config.base_denom);
+
+    assert_eq!(signer_strategy_denom_balance, Uint128::from(0u128));
+    assert_eq!(contract_base_denom_balance, Uint128::from(0u128));
 }
